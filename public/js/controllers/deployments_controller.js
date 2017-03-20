53fff72a894af4fdf26808d0d91c1f33fc2fecd9
Adrian Mui <amui@omeda.com>
2017-03-20 09:57:58 -0500

    added selectDeployment controller function to trigger another ajax call

--- public/js/controllers/deployments_controller.js
+++ public/js/controllers/deployments_controller.js
@@ -6,6 +6,7 @@ app.controller('DeploymentCtrl', ($scope, DeploymentService) => {
   console.log('DeploymentCtrl');
 
   $scope.deployments = [];
+  $scope.selected;
 
   // treat this as NgOnInit() in Angular 2
   DeploymentService.ajaxDeployments()
@@ -15,8 +16,22 @@ app.controller('DeploymentCtrl', ($scope, DeploymentService) => {
       $scope.$apply();
   });
 
-  $scope.hello = ()=> {
+  $scope.hello = () => {
     debugger;
+  };
+
+  $scope.selectDeployment = ($event) => {
+    let $node = $($event.currentTarget);
+
+    if (!$scope.selected) {
+      $scope.selected = $node;
+    }  else {
+      // if selected another $node
+      $scope.selected.toggleClass('a123').toggleClass('b123');
+      $scope.selected = $node;
+    }
+
+    $scope.selected.toggleClass('a123').toggleClass('b123');
   }
 });
 
